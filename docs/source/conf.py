882f83588d45a969010f4076a88b3499ad2331a7
r1cardoPereira <ricardo.pereira_jr@outlook.com>
2023-09-15 14:55:41 -0300

    teste

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Adicione o caminho do seu projeto Python para o sys.path
+sys.path.insert(0, os.path.abspath('../'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,7 +20,11 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -25,4 +35,4 @@ language = 'pt-BR'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['docs/build/_static']
+html_static_path = ['_static']
