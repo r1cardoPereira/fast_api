cf225bdb381aa9628c4b113ae9e031f08b304895
r1cardoPereira <ricardo.pereira_jr@outlook.com>
2023-09-15 14:46:02 -0300

    teste

--- docs/build/_sources/index.rst.txt
+++ docs/build/_sources/index.rst.txt
@@ -1,10 +1,10 @@
-.. Estudos_FastAPI documentation master file, created by
-   sphinx-quickstart on Fri Sep 15 12:17:25 2023.
+.. Estudos sobre FastAPI documentation master file, created by
+   sphinx-quickstart on Fri Sep 15 14:43:48 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Estudos_FastAPI's documentation!
-===========================================
+Welcome to Estudos sobre FastAPI's documentation!
+=================================================
 
 .. toctree::
    :maxdepth: 2
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,24 +1,3 @@
-import sys
-import os
-import shutil
-
-# Obter o caminho absoluto dos arquivos de estilo
-pygments_css = os.path.abspath('../build/_static/pygments.css')
-alabaster_css = os.path.abspath('../build/_static/alabaster.css')
-basics_css = os.path.abspath('../build/_static/basic.css')
-
-# Construir o caminho para a pasta _static dentro de build/html
-build_static_path = os.path.join(os.path.abspath('../build/html'), '_static')
-
-# Certificar-se de que a pasta build/html/_static existe
-os.makedirs(build_static_path, exist_ok=True)
-
-# Copiar os arquivos para a pasta de destino
-shutil.copyfile(pygments_css, os.path.join(build_static_path, 'pygments.css'))
-shutil.copyfile(alabaster_css, os.path.join(build_static_path, 'alabaster.css'))
-shutil.copyfile(basics_css, os.path.join(build_static_path, 'basic.css'))
-
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -27,7 +6,7 @@ shutil.copyfile(basics_css, os.path.join(build_static_path, 'basic.css'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Estudos_FastAPI'
+project = 'Estudos sobre FastAPI'
 copyright = '2023, Ricardo Pereira'
 author = 'Ricardo Pereira'
 release = '1'
@@ -35,17 +14,12 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx_autodoc_typehints'
-]
-
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'pt-br'
+language = 'pt-BR'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
